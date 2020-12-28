44a8e0bdbce08312a018947211e1a510418263a8
UniquelySimilar <consciousexplorer@gmail.com>
2020-12-28 13:52:32 -0700

    tracking current question and answer

--- src/Quiz.vue
+++ src/Quiz.vue
@@ -4,11 +4,13 @@
     <p>Please answer the following {{ numQuestions }} questions, presented one at a time.
       Questions can not be skipped, and you cannot return to a previous question.
     </p>
-    <ul>
-      <li v-for="question in questionsExt" :key="question.id">
-        {{ question.id }} - {{ question.text }}
-      </li>
-    </ul>
+    <hr>
+    <p>{{ currentQuestion.text }}</p>
+    <div v-for="(answer,index) in currentQuestion.answers" :key="index">
+      <input type="radio" :value="answer" v-model="selectedAnswer">
+      <label>{{ answer }}</label>
+    </div>
+    <span>Answer: {{ questionsExt[currentIdx].answer }}</span>
   </div>
 </template>
 
@@ -22,26 +24,42 @@ export default {
   },
   data() {
     return {
-      questionsExt: this.addQuestionProperties()
+      questionsExt: this.addQuestionProperties(),
+      currentIdx: 0,
+      selectedAnswer: null
     };
   },
   computed: {
     numQuestions() {
       return this.questions.length;
     },
-    // questionsExt() {
-    //   return this.addQuestionProperties()
-    // }
+    currentQuestion() {
+      return this.questionsExt[this.currentIdx];
+    }
+  },
+  watch: {
+    selectedAnswer() {
+      this.questionsExt[this.currentIdx].answer = this.selectedAnswer;
+    }
   },
   methods: {
     addQuestionProperties() {
-      let questionsExt = this.questions.map( (question, index) => {
-        let questionExt = { ...question, id: index + 1, answer: null };
+      let questionsExt = this.questions.map( (question) => {
+        let questionExt = { ...question, answer: null };
         return questionExt;
       });
 
       return questionsExt;
-    }    
+    },
+    selectAnswer(event) {
+      console.log(event);
+    }
   }
 }
 </script>
+
+<style scoped>
+  input[type="radio"] {
+    margin-right: 1em;
+  }
+</style>
